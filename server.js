464d02bf89efe93944eb82fd607363d4a70f8a32
Brovu <roy772003@gmail.com>
2024-01-01 19:41:29 +0700

    fix bug 2nd

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const multer = require("multer");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
+
 const path = require("path");
 const app = express();
 const port = 3300;
@@ -9,6 +11,7 @@ const port = 3300;
 const ImageModel = require("./image.model");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "dist"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -139,7 +142,6 @@ app.post("/images/:id/share", async (req, res) => {
     // Cập nhật trường shareable thành true
     image.shareable = true;
     await image.save();
-
   } catch (err) {
     console.log("Server Error:", err);
     res.status(500).send("Internal Server Error");
@@ -149,3 +151,7 @@ app.post("/images/:id/share", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
